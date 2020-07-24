bc1d0ef0f6811446a5ee8dafaaaee4e050a1959f
MauBocanegraTorres <55457556+MauBocanegraTorres@users.noreply.github.com>
2020-07-24 04:18:48 -0500

    Implement MVP contract [AAG-12] (#49)

---
+++ mvp_example/src/main/java/com/distillery/android/blueprints/mvp/todo/contract/TodoContract.kt
@@ -0,0 +1,17 @@
+package com.distillery.android.blueprints.mvp.todo.contract
+
+import com.distillery.android.domain.models.ToDoModel
+
+interface TodoContract {
+    interface View {
+        fun showError(message: String)
+        fun notifyTaskDeleted()
+        fun showPendingTasks(tasks: List<ToDoModel>)
+        fun showDoneTasks(tasks: List<ToDoModel>)
+    }
+    interface Presenter {
+        fun onClickAddTask()
+        fun onClickCheckboxCompletion(item: ToDoModel)
+        fun onClickDeleteTask(item: ToDoModel)
+    }
+}
